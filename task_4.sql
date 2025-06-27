9a6c0146509196aecc4237ac9ebc0efce7e49b71
Rama Sheren <ramasheren2004@gmail.com>
2025-06-27 22:44:47 +0300

    done

--- task_4.sql
+++ task_4.sql
@@ -1,16 +1,6 @@
--- Use the specified database
 USE alx_book_store;
 
--- Get full column description of 'books' table without using DESCRIBE or ANALYZE
-SELECT 
-    COLUMN_NAME AS 'Column',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Nullable',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_NAME = 'books'
-    AND TABLE_SCHEMA = 'alx_book_store';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'books'
+  AND TABLE_SCHEMA = 'alx_book_store';
