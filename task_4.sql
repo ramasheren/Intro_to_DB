1030ddebc9d9d5fdaf100d6d67444e80437f52d7
Rama Sheren <ramasheren2004@gmail.com>
2025-06-27 22:40:15 +0300

    done

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,14 @@
+-- Use the correct database
 USE alx_book_store;
 
+-- Get full description of the 'books' table without using DESCRIBE or EXPLAIN
 SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
